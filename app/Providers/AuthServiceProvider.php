4449015e702ec34bf5abebe492e0d4fc37c6f5f2
Gustavo Lazzari <gustavotlazzari@gmail.com>
2025-06-08 22:36:14 -0300

    added policies

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\Patient;
+use App\Policies\PatientPolicy;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        Patient::class => PatientPolicy::class,
     ];
 
     /**
